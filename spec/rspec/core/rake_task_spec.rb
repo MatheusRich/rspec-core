rspec/rspec-metagem

commit ea70e4edbbc52846dcd63308ee349166bbcef29c

prepend 'bundle exec' in rake task
- Fixes #426.

lib/rspec/core/rake_task.rb
@@ -1,5 +1,3 @@
-#!/usr/bin/env ruby
-
 require 'rspec/core'
 require 'rspec/core/deprecation'
 require 'rake'
@@ -133,11 +131,12 @@ module RSpec
               puts "No examples matching #{pattern} could be found"
             else
               begin
-                ruby(spec_command)
+                puts spec_command if verbose
+                success = system(spec_command)
               rescue
                 puts failure_message if failure_message
-                raise("ruby #{spec_command} failed") if fail_on_error
               end
+              raise("ruby #{spec_command} failed") if fail_on_error unless success
             end
           end
         end
@@ -155,10 +154,12 @@ module RSpec
 
       def spec_command
         @spec_command ||= begin
-                            cmd_parts = [ruby_opts]
+                            cmd_parts = []
+                            cmd_parts << "bundle exec" if gemfile? unless skip_bundler
+                            cmd_parts << RUBY
+                            cmd_parts << ruby_opts
                             cmd_parts << "-w" if warning?
                             cmd_parts << "-S"
-                            cmd_parts << "bundle exec" if gemfile? unless skip_bundler
                             cmd_parts << runner
                             if rcov
                               cmd_parts << ["-Ispec#{File::PATH_SEPARATOR}lib", rcov_opts]

spec/rspec/core/rake_task_spec.rb
@@ -5,6 +5,10 @@ module RSpec::Core
   describe RakeTask do
     let(:task) { RakeTask.new }
 
+    def ruby
+      FileUtils::RUBY
+    end
+
     before do
       File.stub(:exist?) { false }
     end
@@ -26,7 +30,7 @@ module RSpec::Core
 
     context "default" do
       it "renders rspec" do
-        spec_command.should =~ /^-S rspec/
+        spec_command.should =~ /^#{ruby} -S rspec/
       end
     end
 
@@ -60,7 +64,7 @@ module RSpec::Core
     context "with rcov" do
       it "renders rcov" do
         with_rcov do
-          spec_command.should =~ /^-S rcov/
+          spec_command.should =~ /^#{ruby} -S rcov/
         end
       end
     end
@@ -69,7 +73,7 @@ module RSpec::Core
       it "renders bundle exec rcov" do
         with_bundler do
           with_rcov do
-            spec_command.should =~ /^-S bundle exec rcov/
+            spec_command.should =~ /^bundle exec #{ruby} -S rcov/
           end
         end
       end
@@ -78,7 +82,7 @@ module RSpec::Core
     context "with ruby options" do
       it "renders them before -S" do
         task.ruby_opts = "-w"
-        spec_command.should =~ /^-w -S rspec/
+        spec_command.should =~ /^#{ruby} -w -S rspec/
       end
     end
 

Rakefile
@@ -28,7 +28,6 @@ Cucumber::Rake::Task.new(:cucumber)
 desc "Run all examples"
 RSpec::Core::RakeTask.new(:spec) do |t|
   t.rspec_opts = %w[--color]
-  t.verbose = false
 end
 
 if RUBY_VERSION.to_f == 1.8

features/command_line/rake_task.feature
@@ -20,7 +20,7 @@ Feature: rake task
       end
       """
     When I run `rake`
-    Then the stderr should contain "ruby -S rspec"
+    Then the output should contain "ruby -S rspec"
     Then the exit status should be 0
 
   Scenario: default options with failing spec (exit status is 1)

MatheusRich/rspec-core

commit 072237973425f6717c7348503d59f249025dc511

Fix `RSpec::Core::RakeTask#failure_message`.
`system` returns a boolean value to indicate success/failure,
but the use of `failure_message` was in a `rescue` block that
never got executed.  It appears this has been broken since
ea70e4edbbc52846dcd63308ee349166bbcef29c. Before that commit,
we used `Rake::FileUtilsExt#ruby`, which does indicate failure
by raising an error (and thus the `rescue` was correct). In
ea70e4edbbc52846dcd63308ee349166bbcef29c, we switched to using
`system` and the rescue was left in place but never got hit
anymore.

The lack of test coverage here is why we never noticed, so I
addressed that as well.

lib/rspec/core/rake_task.rb
@@ -63,16 +63,12 @@ module RSpec
       # @private
       def run_task(verbose)
         command = spec_command
+        puts command if verbose
 
-        begin
-          puts command if verbose
-          success = system(command)
-        rescue
-          puts failure_message if failure_message
-        end
+        return if system(command)
+        puts failure_message if failure_message
 
-        return unless fail_on_error && !success
-
+        return unless fail_on_error
         $stderr.puts "#{command} failed" if verbose
         exit $?.exitstatus
       end

spec/rspec/core/rake_task_spec.rb
@@ -80,6 +80,23 @@ module RSpec::Core
       end
     end
 
+    context "when `failure_message` is configured" do
+      before do
+        allow(task).to receive(:exit)
+        task.failure_message = "Bad news"
+      end
+
+      it 'prints it if the RSpec run failed' do
+        task.ruby_opts = '-e "exit(1);" ;#'
+        expect { task.run_task false }.to output(/Bad news/).to_stdout
+      end
+
+      it 'does not print it if the RSpec run succeeded' do
+        task.ruby_opts = '-e "exit(0);" ;#'
+        expect { task.run_task false }.not_to output(/Bad/).to_stdout
+      end
+    end
+
     context 'with custom exit status' do
       def silence_output(&block)
         expect(&block).to output(anything).to_stdout.and output(anything).to_stderr